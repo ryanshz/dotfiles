22973da58754db92bf348a98e3be485414982151
ryanshz <92750669+ryanshz@users.noreply.github.com>
2023-10-05 00:51:41 -0400

    Added all files from debian
    10/05/23

---
+++ polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+# polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+killall -q polybar
+
+# Launch main and side
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar side 2>&1 | tee -a /tmp/polybar1.log & disown
