c4e6acbed8c9f583652bf98306c0306c8a92cbeb
ryanshz <92750669+ryanshz@users.noreply.github.com>
2023-10-08 20:43:52 -0400

    switched compiler to picom, fixed like all issues, edited displays after

--- xrandr-monitor-fix/launch.sh
+++ xrandr-monitor-fix/launch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xrandr --output HDMI-1 --primary --mode 1920x1080 --rotate normal --output DP-2 --mode 1920x1080 --rotate normal --right-of HDMI-1
+xrandr --output HDMI-0 --primary --mode 1920x1080 --rotate normal --output DP-2 --mode 1920x1080 --rotate normal --right-of HDMI-0
